54f7d53b9b7d4eb26a89fd33406a1fc7d5d9fe47
Robert Goené <robert@goene.nl>
2025-11-28 16:37:00 +0100

    stuff

--- front/src/index.js
+++ front/src/index.js
@@ -1,4 +1,23 @@
 
+document.addEventListener("DOMContentLoaded", (event) => {
+
+
+
+    document.querySelector("#myUL").addEventListener("click", (e) => {
+	console.log(e.target)
+    })
+    
+
+    document.querySelectorAll("li").forEach( (x) => {
+	x.addEventListener("click", (e) => {
+	    //e.stopPropagation();
+	    //console.log(e.currentTarget)
+	})
+    })
+
+
+    
+})
 
 if (DEV) {
     new EventSource('/esbuild').addEventListener('change', () => location.reload())
